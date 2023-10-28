43d5113b5e832747fbcdcc9382f6e1dc1f7ec480
bosro <misterbosro@gmail.com>
2023-10-28 16:34:45 +0000

    inventoryProject

--- inventoryproject/settings.py
+++ inventoryproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0&cd3#$d%dc68%#fdjegli5(#x+!pa1r%f=ru$j2llm!z@kgf(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
